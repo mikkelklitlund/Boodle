60f1229d5f62445829ded03f906e5ac98a66fb54
Emil Lodberg <elodbe19@student.aau.dk>
2022-03-18 18:57:51 +0100

    changes to js file

--- Coursedata.js
+++ Coursedata.js
@@ -4,7 +4,7 @@ import fs from "fs";
 import fetch from "node-fetch"
 
 const options =
-  "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_course_get_contents&courseid=41300&moodlewsrestformat=xml";
+  "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_course_get_contents&courseid=41300&moodlewsrestformat=json";
 
 async function fetch_data(){
     fetch(options)
@@ -13,6 +13,7 @@ async function fetch_data(){
             return Response.text();
         } else {throw new Error(`fejl status ${response.status}`);
     }})
-    .then(body => fs.writeFileSync("course_data1.json", JSON.stringify(body, null, "\t")));
+    .then(body => fs.writeFileSync("course_data1.json", body));
     };
 fetch_data()
+//
