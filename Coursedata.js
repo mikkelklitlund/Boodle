a93f91eac51742a9cee1d9f0bb4729c6fafb6243
Emil Lodberg <elodbe19@student.aau.dk>
2022-03-21 08:55:47 +0100

    improvements to API calls

--- Coursedata.js
+++ Coursedata.js
@@ -1,19 +1,28 @@
-"use strict"
+"use strict";
 
 import fs from "fs";
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 const options =
-  "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_course_get_contents&courseid=41300&moodlewsrestformat=json";
+  "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_course_get_contents&moodlewsrestformat=json&courseid=41300";
 
-async function fetch_data(){
-    fetch(options)
-    .then(Response => {
-        if (Response.ok){
-            return Response.text();
-        } else {throw new Error(`fejl status ${response.status}`);
-    }})
-    .then(body => fs.writeFileSync("course_data1.json", body));
-    };
-fetch_data()
-//
+async function fetch_data() {
+  fetch(options)
+    .then((Response) => {
+      if (Response.ok) {
+        return Response.text();
+      } else {
+        throw new Error(`fejl status ${response.status}`);
+      }
+    })
+    .then((body) => fs.writeFileSync("course_data1.json", body));
+}
+fetch_data();
+
+function json(){
+    let course_data = fs.readFileSync('course_data1.json');
+    const parsedData = JSON.parse(course_data);
+    console.log(parsedData);
+}
+
+json()
