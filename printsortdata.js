2b580b60ee3cff2b4fcffcb2ed63ccc2882fc718
Emil Lodberg <elodbe19@student.aau.dk>
2022-03-28 12:40:41 +0200

    add sites resourses

---
+++ printsortdata.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+const my_json_file = require("./course_data1.json") // use the require method
+
+
+function reader(){
+  
+   for (let i = 1; i < my_json_file.length; i++){
+       console.log(my_json_file[i].summary);
+   }
+}
+reader()
