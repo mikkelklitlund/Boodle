250c1917a89301b3eaf0000c8a7f0f7099b776bc
Emil Lodberg <elodbe19@student.aau.dk>
2022-04-07 08:48:01 +0200

    saves

--- fetchCoursedata/commingCourses.js
+++ fetchCoursedata/commingCourses.js
@@ -4,7 +4,7 @@ const { default: axios } = require('axios');
 const { connectDB } = require('../database/connectDB');
 const { fetchUser } = require('../database/manageUserDB');
 const {reader} = require('./printsortdata');
-
+const {exportTest, input_reciver} = require('./discordBot/index')
 let id_array = [];
 
 connectDB();
@@ -29,7 +29,7 @@ async function commingCourses() {
 commingCourses();
 
 async function fetch_data() {
-    var options = "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_course_get_contents&moodlewsrestformat=json&courseid=" + "41300";
+    var options = "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=" + moodle_token +  "&wsfunction=core_course_get_contents&moodlewsrestformat=json&courseid=" + "41300";
     
     fetch(options)
       .then((Response) => {
