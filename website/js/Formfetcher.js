16278434de1fec2573608ff5fd584874ee531ade
mikkelklitlund <mik.kkel@hotmail.com>
2022-04-08 13:15:39 +0200

    more clean-up

--- website/js/Formfetcher.js
+++ website/js/Formfetcher.js
@@ -3,11 +3,11 @@ let btnSubmit = document.querySelector('#btnSubmit');
 
 btnSubmit.addEventListener("click", async () => {
   let PostMoodletoken = document.querySelector("#PostMoodletoken");
-  let PostDiscordid = document.querySelector("#PostDiscordid");
+  //let PostDiscordid = document.querySelector("#PostDiscordid");
 
   let formpost = {
     Moodle: PostMoodletoken.value,
-    Discord: PostDiscordid.value,
+    //Discord: PostDiscordid.value,
   };
 
   let post = await fetch("https://fs-21-sw-2-a219a.p2datsw.cs.aau.dk/node0/", {
