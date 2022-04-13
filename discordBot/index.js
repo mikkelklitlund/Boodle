34108a4869fc7675fdf289320a4688c9e3261ef4
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2022-04-13 21:15:14 +0200

    Post moodle token
    skal bare sendes til db og så er den ged barberet

--- discordBot/index.js
+++ discordBot/index.js
@@ -2,31 +2,31 @@
 const fs = require("node:fs");
 require("dotenv").config();
 const { Client, Collection, Intents } = require("discord.js");
+
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const { body, validationResult } = require("express-validator");
 
+// const { createUser } = require("../database/manageUserDB");
 
 let moodleToken;
 //explicitly added hostname
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 // Arbitrary port, should be between 3090-3099
 const port = 3090;
 // Initialization of Express
 const app = express();
 // For making files accessible in directory
 app.use("/public", express.static("../website"));
-app.set('views',path.join(process.cwd(), '../website/resources'));
-app.set('view engine', 'pug');
+app.set("views", path.join(process.cwd(), "../website/resources"));
+app.set("view engine", "pug");
 
 // Testing
 app.get("/", (req, res) => {
   // res.send('TisTest');
-  res.sendFile(
-    path.join(__dirname, '..','website/resources/webpage.html')
-  );
+  res.sendFile(path.join(__dirname, "..", "website/resources/webpage.html"));
 });
-
 
 app.post("/", (req, res) => {
   console.log("post");
@@ -47,32 +47,46 @@ app.post("/", (req, res) => {
   // res.send("POST tis test");
 });
 
-
 // Path for registration of Moodle token
-app.get("/register/:id", (req, res) => {
+app.get("/register/:id/", (req, res) => {
   // res.send(
   //   "Discord id: " +
   //     Buffer.from(req.params.id, "base64").toString("ascii") +
   //     `Discord tag: ${userTag}`
   // );
   // res.sendFile(path.join(__dirname,"..website/resources/webpage.html"));
-  res.render('webpage.pug',{id: Buffer.from(req.params.id,'base64').toString('utf-8') });
+  res.render("webpage.pug", {
+    id: Buffer.from(req.params.id, "base64").toString("utf-8"),
+  });
 
   // TODO: Lav register side.
 });
+// ?name=:name
+app.post("/register/:id/", (req, res) => {
+  console.log(
+    `POST request from ${Buffer.from(req.params.id, "base64").toString(
+      "utf-8"
+    )}`
+  );
 
-app.post("/regiser/:id", (req, res) => {
-  console.log(`POST request from ${Buffer.from(req.params.id,'base64').toString('utf-8')}`);
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    res.status("400").send("Input validation failed");
+  }
 
   let body = "";
-
-  req.on('data', (data) => {
+  req.on("data", (data) => {
     body += data;
   });
 
-  req.on('end', () => {
-    // TODO: Send til DB
-    console.log('POST request ended');
+  req.on("end", () => {
+    const discordId = body.substring(body.indexOf("=") + 1, body.indexOf("&"));
+    const MoodleToken = body.substring(body.lastIndexOf("=") + 1, body.length);
+    console.log(
+      `POST request ended: DiscordId: ${discordId} Moodletoken: ${MoodleToken}`
+    );
+    // TODO: Send til DB og check om bruger findes i forvejen
+    res.end();
   });
 });
 
@@ -82,7 +96,7 @@ app.get("/Boodle", (req, res) => {
 }); */
 
 // Creates HTTPS server
-const server = http.createServer(app).listen(port,hostname, () => {
+const server = http.createServer(app).listen(port, hostname, () => {
   console.log(`Server Running at http://localhost:${port}`);
 });
 
@@ -145,4 +159,4 @@ client.on("interactionCreate", async (interaction) => {
 client.login(process.env.BOT_TOKEN);
 // console.log(Buffer.from('din mor').toString('base64')); // din mor -> ZGluIG1vcg==
 // console.log(Buffer.from('ZGluIG1vcg==','base64').toString('utf8')); // ZGluIG1vcg== -> din mor
-module.exports = {moodleToken}
+module.exports = { moodleToken };
---
+++ website/resources/js/Formfetcher.js
@@ -0,0 +1,18 @@
+// let formSubmit = document.querySelector('#formSubmit');
+// let btnSubmit = document.querySelector('#btnSubmit');
+// //! lav om
+// btnSubmit.addEventListener("click", async () => {
+//   let PostMoodletoken = document.querySelector("#PostMoodletoken");
+//   //let PostDiscordid = document.querySelector("#PostDiscordid");
+
+//   let formpost = {
+//     Moodle: PostMoodletoken.value,
+//     //Discord: PostDiscordid.value,
+//   };
+// // http://localhost:3090/register
+//   let post = await fetch(window.location.href, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json;charset=utf-8" },
+//     body: JSON.stringify(formpost),
+//   });
+// });
