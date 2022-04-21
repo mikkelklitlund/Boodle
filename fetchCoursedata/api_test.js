ced94125a2c66b1e60208209e7a8d60f7b0bea22
Emil Lodberg <elodbe19@student.aau.dk>
2022-04-21 10:03:43 +0200

    big changes in mf code

--- fetchCoursedata/api_test.js
+++ fetchCoursedata/api_test.js
@@ -1,32 +1,65 @@
 const { response } = require("express");
 const fs = require("fs");
 const fetch = require("node-fetch");
-const moodle_token = "fea55e838143611e65bdaef0a6c1e2b0"
 
-var course = {
-    courseName: [],
-    course_date_unix: [],
-    course_date: [],
-    summary: [],
-    location: [],
-    course_links: []
-}
+const courseid = "41259";
+const moodle_token = "fea55e838143611e65bdaef0a6c1e2b0";
 
-var url = 'https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_calendar_get_calendar_events&moodlewsrestformat=json&events[courseids][0]=41300&options[timestart]=1640998800&options[timeend]=1656637200'
+let course = {
+  Event: [
+    {
+      courseName: [],
+      course_description: [],
+      course_date_unix: [],
+      course_date: [],
+      summary: [],
+      location: [],
+      course_links: [],
+    },
+  ],
+};
 
-async function course_module_event(){
-    fetch(url)
-    .then((req) => {
-        if (req.ok) {
-            return req.text()
-        } else {
-            throw new Error(`fejl status ${req.status}`);
-        }
-    })
-    .then((res) => { 
-    for(let i in res.data.events){
-        course_date[i] = 
-    }
+async function course_module_event(moodle_token, courseid) {
+  var url =
+    "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=" +
+    moodle_token +
+    "&wsfunction=core_calendar_get_calendar_events&moodlewsrestformat=json&events[courseids][0]=" +
+    courseid +
+    "&options[timestart]=1640998800&options[timeend]=1656637200";
+
+  fetch(url)
+    .then((req) => req.json())
+    .then((json) => {
+      for (let i in json.events) {
+        course.Event[0].course_date_unix[i] = json.events[i].timestart;
+        course.Event[0].course_description[i] = json.events[i].name;
+
+        console.log(course.Event[0].course_date_unix[i]);
+      }
     });
 }
-course_module_event();
+
+async function fetch_data(moodle_token, courseid) {
+  var options =
+    "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=" +
+    moodle_token +
+    "&wsfunction=core_course_get_contents&moodlewsrestformat=json&courseid=" +
+    courseid;
+
+  fetch(options)
+    .then((req) => req.json())
+    .then((json) => {
+      if (Number(json[2].id) < Number(json[3].id)) {
+        for (let i in json) {
+          course.Event[0].summary[i] = json[i].summary;
+        }
+      }else if (Number(json[2].id > Number(json[3].id))){
+          for (let j = json.length; j > 0; j--){
+              course.Event[0].summary[j] = json[j].summary
+          }
+      }
+    });
+}
+
+course_module_event(moodle_token, courseid);
+fetch_data(moodle_token, courseid);
