d6992bd37c36c72856090bdcaa4e72e6b57102d4
mikkelklitlund <mik.kkel@hotmail.com>
2022-04-22 09:46:52 +0200

    digital ocean server

--- discordBot/commands/createUser.js
+++ discordBot/commands/createUser.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription('Generates and sends link to input Moodle token'),
     async execute(interaction) {
         // Converts user id to base64 for url generation
-        await interaction.reply({ content: `http://165.22.18.133//register/${Buffer.from(interaction.user.id, 'ascii').toString('base64')}/\nPlease follow the link and input your Moodle token`, ephemeral: true});
+        await interaction.reply({ content: `http://165.22.18.133/register/${Buffer.from(interaction.user.id, 'ascii').toString('base64')}/\nPlease follow the link and input your Moodle token`, ephemeral: true});
         //await interaction.reply({ content: `https://fs-21-sw-2-a219a.p2datsw.cs.aau.dk/node0/register/${Buffer.from(interaction.user.id, 'ascii').toString('base64')}/\nPlease follow the link and input your Moodle token`, ephemeral: true});
         // https://fs-21-sw-2-a219a.p2datsw.cs.aau.dk/node0/register/${Buffer.from(interaction.user.id, 'ascii').toString('base64')}
     },
