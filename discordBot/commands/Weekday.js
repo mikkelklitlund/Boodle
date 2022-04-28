d7474ca192047ae08150f7adef756113cd433b7e
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2022-04-28 15:25:30 +0200

    12345678iolkjhgfdsz<

--- discordBot/commands/Weekday.js
+++ discordBot/commands/Weekday.js
@@ -1,8 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { calendarDayView, getNextWday, datePToObj } = require("../../fetchCoursedata/calendarGetDayView.js");
 const { fetchUser } = require("../../database/manageUserDB.js");
-const { date } = require("zod");
-const { throwStatement } = require("@babel/types");
+const { MessageEmbed } = require("discord.js");
 
 function fullDate() {
     const weekday = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
@@ -38,8 +37,9 @@ module.exports = {
         await interaction.deferReply();
         let dateOBJ;
         let currentDate = fullDate();
-        let user = fetchUser(interaction.user.id);
-        calendarDayView(user.moodle_token, currentDate.day, currentDate.month, currentDate.year);
+        // TODO Ryd op
+        // let user = fetchUser(interaction.user.id);
+        // calendarDayView(user.moodle_token, currentDate.day, currentDate.month, currentDate.year);
         // const dinfar = CommandInteractionOptionResolver.getString("weekday");
         switch (interaction.options.data[0].value) {
             case "monday":
@@ -69,10 +69,23 @@ module.exports = {
         }
         console.log(dateOBJ);
         calendarDayView('c4043b1ff4ed72d98f8107586f61e4cb',dateOBJ.day,dateOBJ.month,dateOBJ.year)
-        .then(res => JSON.stringify(res))
-        .then(res => {
-            console.log(res);
-            interaction.editReply(res);
+        // .then(res => JSON.stringify(res))
+        .then(async res => {
+            // console.log(interaction.options.data[0].value);
+            // console.log(JSON.stringify(res));
+            let embedList = [];
+            res.forEach((element, i) => {
+                let embed = new MessageEmbed({
+                    title: element.fullname,
+                    url: element.url,
+                    description: element.description
+                });
+                embedList[i] = embed;
+                console.log(`i=${i}`,embed);
+            });
+            console.log(embedList.length);
+            // await interaction.editReply(`URL: ${res[0].url}`);
+           await interaction.editReply({embeds: embedList});
         })
         .catch(err => console.error(err));
         // .then( async res => await interaction.editReply(res)); 
