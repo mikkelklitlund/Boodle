fab258168415568102d280dcdb93cfc0d4bc4b71
ZumzarHS <94530862+ZumzarHS@users.noreply.github.com>
2022-05-05 08:29:00 +0200

    test
    changed to new test

--- test/SortingSummary.test.js
+++ test/SortingSummary.test.js
@@ -1,24 +1,31 @@
-const {SortHTML} = require ('../fetchCoursedata/SortingSummary');
+const {html_to_string} = require ('../fetchCoursedata/SortingSummary');
 
 test ('HTML to JavaScript', () => {
   /* HTML linebreak to JS linebreak */
-  const linebreak = 'Replace HTML linebreak <br> to JS linebreak';
-  expect (SortHTML (linebreak)).toEqual (
+  const linebreak = '<p>Replace HTML linebreak <br> to JS linebreak</p>';
+  expect (html_to_string (linebreak)).toEqual (
     'Replace HTML linebreak \n to JS linebreak'
   );
   /* Remove tags */
   const removetags = '<p>Remove tags</p>';
-  expect (SortHTML (removetags)).toEqual ('Remove tags');
+  expect (html_to_string (removetags)).toEqual ('Remove tags');
   /* Fixes ampersand */
-  const amp = 'HTML &amp; JavaScript';
-  expect (SortHTML (amp)).toEqual ('HTML & JavaScript');
-  /* Change non-breaking-whitespace to whitespace */
-  const whitespace = 'blabla&nbsp;blabla';
-  expect (SortHTML (whitespace)).toEqual ('blabla blabla');
+  const amp = '<p><h5>HTML &amp; JavaScript</h5></p>';
+  expect (html_to_string (amp)).toEqual ('HTML & JavaScript');
+  /* Change list to whitespace */
+  const list = '<p>blabla<li>blabla</li></p>';
+  expect (html_to_string (list)).toEqual ('blabla blabla');
   /* Removes double whitespace */
-  const doublewhitespaces = 'Remove       double  whitespace';
-  expect (SortHTML (doublewhitespaces)).toEqual ('Remove double whitespace');
+  const doublewhitespaces = '<p>Remove      double    whitespace</p>';
+  expect (html_to_string (doublewhitespaces)).toEqual (
+    'Remove double whitespace'
+  );
+  /* Double newlines before headers */
+  const header = '<p>Test<h1>Hello World</h1></p>';
+  expect (html_to_string (header)).toEqual ('Test\n\nHello World');
   /* Remove whitespace in beginning and end of string */
-  const trimstring = ' Remove whitespace in start and end ';
-  expect (SortHTML (trimstring)).toEqual ('Remove whitespace in start and end');
+  const trimstring = ' <p>Remove whitespace in start and end</p> ';
+  expect (html_to_string (trimstring)).toEqual (
+    'Remove whitespace in start and end'
+  );
 });
