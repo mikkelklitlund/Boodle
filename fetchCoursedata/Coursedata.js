70ff04484cb072899ed0cd69ddb026d8911d2a13
BlakRoenne <94530885+BlakRoenne@users.noreply.github.com>
2022-05-10 08:38:22 +0200

    minor change to sorting summary

---
+++ fetchCoursedata/Coursedata.js
@@ -0,0 +1,24 @@
+"use strict";
+const fs = require("fs");
+const fetch = require("node-fetch");
+const my_json_file = require("./course_data1.json");
+
+
+
+
+async function fetch_data(courseid) {
+  var options = "https://www.moodle.aau.dk/webservice/rest/server.php?wstoken=fea55e838143611e65bdaef0a6c1e2b0&wsfunction=core_course_get_contents&moodlewsrestformat=json&courseid=" + courseid;
+  
+  fetch(options)
+    .then((Response) => {
+      if (Response.ok) {
+        return Response.text();
+      } else {
+        throw new Error(`fejl status ${response.status}`);
+      }
+    })
+    .then((body) => fs.writeFileSync("course_data1.json", body));
+}
+
+
+module.exports = {fetch_data}
