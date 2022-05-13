ae7f732b9460c1316263b524ce6984fb0e12b576
mikkelklitlund <mik.kkel@hotmail.com>
2022-05-13 22:30:09 +0200

    added specific date

---
+++ discordBot/commands/specificDate.js
@@ -0,0 +1,114 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+	calendarDayView
+} = require("../../fetchCoursedata/calendarGetDayView.js");
+const { fetchUser } = require("../../database/manageUserDB.js");
+const { MessageEmbed } = require("discord.js");
+const { assembler } = require("../../fetchCoursedata/BoodleCourseHandler.js");
+const { html_to_string } = require("../../fetchCoursedata/SortingSummary");
+const { syncModules } = require("../../helpers/syncModules");
+const { customEmbedField } = require("../../helpers/customEmbedField");
+const { validateDate } = require("../../helpers/validation");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("show")
+		.setDescription("Prints schedule for a given date")
+		.addStringOption((option) =>
+			option
+				.setName("date")
+				.setDescription("Prints schedule for a specific date (dd/mm/yyyy).")
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		//Validate date
+		if (!validateDate(interaction.options.data[0].value)) {
+			await interaction.editReply(
+				"Date is not valid! Remember format: dd/mm/yyyy"
+			);
+			return;
+		}
+
+		let dateOBJ = {
+			day: interaction.options.data[0].value.slice(0, 2),
+			month: interaction.options.data[0].value.slice(3, 5),
+			year: interaction.options.data[0].value.slice(6, 10)
+		};
+
+		let summary;
+		await fetchUser(interaction.user.id)
+			.then(async (res) => {
+				if (!res) {
+					await interaction.editReply("No moodle token found");
+					return;
+				} else {
+					summary = await assembler(
+						res.moodle_token,
+						dateOBJ.day,
+						dateOBJ.month,
+						dateOBJ.year
+					);
+					return calendarDayView(
+						res.moodle_token,
+						dateOBJ.day,
+						dateOBJ.month,
+						dateOBJ.year
+					);
+				}
+			})
+			.then(async (res) => {
+				let bigField = [];
+				let nextModule = syncModules(
+					summary,
+					interaction.options.data[0].value
+				);
+				let summaryCounter = 0;
+				res.forEach((element, i) => {
+					let field = new customEmbedField(
+						element.instanceName,
+						html_to_string(JSON.stringify(element.description)),
+						element.location,
+						element.time,
+						element.fullname,
+						element.url,
+						html_to_string(
+							JSON.stringify(
+								summary[summaryCounter].events[nextModule[summaryCounter]]
+									.courseData
+							)
+						)
+					).fieldEmbedifier();
+					let tempArr = [];
+					tempArr[i] = field;
+					bigField = bigField.concat(tempArr[i]);
+					if (
+						typeof res[i + 1] == "undefined" ||
+						res[i].fullname !== res[i + 1].fullname
+					) {
+						//Need to check whether it is a "double course/module" or not
+						summaryCounter++;
+					}
+				});
+				bigField.pop();
+				let bigEmbed = new MessageEmbed({
+					title: res.length < 2 ? "Course" : "Courses",
+					url: "https://moodle.aau.dk/my/",
+					fields:
+						bigField.length == 0
+							? [
+									{
+										name: "No courses",
+										value: `No courses found at ${interaction.options.data[0].value}`,
+										inline: false
+									}
+							  ]
+							: bigField
+				});
+				await interaction.editReply({ embeds: [bigEmbed] });
+			})
+			.catch((err) => console.error(err));
+	}
+};
