cb7c99fb4deb53c87a860a34daa9e04f5ce46c79
mikkelklitlund <mik.kkel@hotmail.com>
2022-06-27 11:10:44 +0200

    fixed too long embeds

--- discordBot/server.js
+++ discordBot/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const registerRoute = require("./routes/register.js");
 const aboutRoute = require("./routes/about.js");
